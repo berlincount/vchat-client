c9e63d4003a58f41367fef1f0672756cbb44ddfa
Dirk Engling <erdgeist@erdgeist.org>
2023-04-21 23:48:36 +0200

    Fix prototype

--- vchat-connection.h
+++ vchat-connection.h
@@ -5,7 +5,7 @@
 int vc_connect(const char *host, const char *port);
 void vc_sendmessage(const char *message);
 int vc_receive();
-int vc_poll();
+int vc_poll(int);
 void vc_disconnect();
 
 const char *vchat_tls_version_external();
